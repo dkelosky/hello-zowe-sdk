aa54a98af962e18c45ef76bff13f10c4d586dbd3
Dan Kelosky <dkelosky@gmail.com>
2021-04-21 13:48:55 -0400

    initial
    Signed-off-by: Dan Kelosky <dkelosky@gmail.com>

--- index.ts
+++ index.ts
@@ -10,13 +10,14 @@ import { GetJobs, IJob } from "@zowe/zos-jobs-for-zowe-sdk";
     //Initialize the Imperative Credential Manager Factory and Logger
     Logger.initLogger(LoggingConfigurer.configureLogger('lib', { name: 'test' }));
     // Uncommment the below line if the Secure Credential Store is in use
-    // await CredentialManagerFactory.initialize({service: "Zowe-Plugin"});
+    await CredentialManagerFactory.initialize({service: "Zowe-Plugin"});
 
     // Get the default z/OSMF profile and create a z/OSMF session with it
     let defaultZosmfProfile: IProfile;
     try {
         defaultZosmfProfile = await getDefaultProfile("zosmf", true);
     } catch (err) {
+        console.log(err)
         throw new ImperativeError({ msg: "Failed to get a profile." });
     }
 
